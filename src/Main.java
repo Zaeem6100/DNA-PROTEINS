ec7abedd732814317910e2404d0cc56445d5def2
Zaeem <32678310+Zaeem6100@users.noreply.github.com>
2021-10-28 16:55:28 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String st = input.next();
+        st = st.toUpperCase();
+        List<String> list  = SplitString(st);
+        System.out.println(list);
+    }
+
+    private static List<String> SplitString(String st){
+        List<String> list = new ArrayList<>();
+        int j=0;
+        String temp ="";
+        for (int i = 0; i < st.length(); i++) {
+            if (j>2){
+                temp= temp+st.charAt(i);
+                list.add(temp);
+                j=0;
+                temp = "";
+            }else {
+                temp= temp+st.charAt(i);
+                j++;
+            }
+        }
+        return list;
+    }
+}
---
+++ src/input.java
@@ -0,0 +1,110 @@
+import com.sun.jdi.Value;
+
+import java.util.HashMap;
+import java.util.List;
+
+class value{
+    private  String Condon;
+    private String Amino;
+
+    public value(String condon, String amino) {
+        Condon = condon;
+        Amino = amino;
+    }
+
+    public String getCondon() {
+        return Condon;
+    }
+
+    public void setCondon(String condon) {
+        Condon = condon;
+    }
+
+    public String getAmino() {
+        return Amino;
+    }
+
+    public void setAmino(String amino) {
+        Amino = amino;
+    }
+}
+
+public class input {
+    HashMap<String, value >map;
+    public void  inp(){
+        map.put("UUU",new value("Phe","F"));
+        map.put("UUC",new value("Phe","F"));
+        map.put("UUA",new value("Leu","L"));
+        map.put("UUG",new value("Leu","L"));
+        map.put("CUU",new value("Leu","L"));
+        map.put("CUC",new value("Leu","L"));
+        map.put("CUA",new value("Leu","L"));
+        map.put("CUG",new value("Leu","L"));
+        map.put("AUU",new value("ILe","I"));
+        map.put("AUC",new value("ILe","I"));
+        map.put("AUA",new value("LLe","I"));
+        map.put("AUG",new value("Met","M"));
+        map.put("GUU",new value("Val","V"));
+        map.put("GUC",new value("Val","V"));
+        map.put("GUA",new value("Val","V"));
+        map.put("GUG",new value("Val","V"));
+        map.put("UCU",new value("Ser","S"));
+        map.put("UCC",new value("Ser","S"));
+        map.put("UCA",new value("Ser","S"));
+        map.put("UCG",new value("Ser","S"));
+        map.put("CCU",new value("Pro","P"));
+        map.put("CCC",new value("Pro","P"));
+        map.put("CCA",new value("Pro","P"));
+        map.put("CCG",new value("Pro","P"));
+        map.put("ACU",new value("Phr","T"));
+        map.put("ACC",new value("Phr","T"));
+        map.put("ACA",new value("Phr","T"));
+        map.put("ACG",new value("Phr","T"));
+        map.put("GCU",new value("Ala","A"));
+        map.put("GCC",new value("Ala","A"));
+        map.put("GCA",new value("Ala","A"));
+        map.put("GCG",new value("Ala","A"));
+        map.put("UAU",new value("Tyr","T"));
+        map.put("UAC",new value("Tyr","T"));
+        map.put("UAA",new value("Stop","Stop"));
+        map.put("UAG",new value("Stop","Stop"));
+        map.put("UGA",new value("Stop","Stop"));
+        map.put("CAU",new value("His","H"));
+        map.put("CAC",new value("His","H"));
+        map.put("CAA",new value("Gln","Q"));
+        map.put("CAG",new value("Gln","Q"));
+        map.put("AAU",new value("Asn","N"));
+        map.put("AAC",new value("Asn","N"));
+        map.put("AAA",new value("Lys","K"));
+        map.put("AAG",new value("Lys","K"));
+        map.put("GAU",new value("Asp","D"));
+        map.put("GAC",new value("Asp","D"));
+        map.put("GAA",new value("Glu","E"));
+        map.put("GAG",new value("Glu","E"));
+        map.put("UGU",new value("Cys","C"));
+        map.put("UGC",new value("Cys","C"));
+        map.put("UGG",new value("Trp","W"));
+        map.put("CGU",new value("Arg","R"));
+        map.put("CGC",new value("Arg","R"));
+        map.put("CGA",new value("Arg","R"));
+        map.put("CGG",new value("Arg","R"));
+        map.put("AGA",new value("Arg","R"));
+        map.put("AGG",new value("Arg","R"));
+        map.put("AGU",new value("Ser","S"));
+        map.put("AGC",new value("Ser","S"));
+        map.put("GGU",new value("Gly","G"));
+        map.put("GGC",new value("Gly","G"));
+        map.put("GGA",new value("Gly","G"));
+        map.put("GGG",new value("Gly","G"));
+    }
+}
+
+
+
+
+
+
+
+
+
+
